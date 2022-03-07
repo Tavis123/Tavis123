4ead6ff62bf3e96698a2dbcada2e6035e342b0aa
Tavis123 <101103782+Tavis123@users.noreply.github.com>
2022-03-07 18:34:51 +0800

    Add files via upload

---
+++ SetMeal.java
@@ -0,0 +1,14 @@
+package 西二炸鸡店;
+//套餐类
+public class SetMeal {
+    private String dishname;
+    private double price;
+    private String friedchickenname;
+    private int left;
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
